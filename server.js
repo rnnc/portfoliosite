e0e211547c4905ec8f893b0a10ef90614289dde2
rnnc <rohan_cy@outlook.com>
2018-09-29 11:41:26 -0400

    preparing to deploy

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'))
-})
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log('Listening on port ', PORT);
