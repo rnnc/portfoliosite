f38ab6485f4690042e57c1d061c035c016b913b8
rnnc <rohan_cy@outlook.com>
2018-10-05 11:47:05 -0400

    added invalid endpoint

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+app.get('/', (req, res)=>{
+  res.send(`Invalid Enpoint\n`);
+})
+
 app.listen(PORT, () => {
   console.log('Listening on port ', PORT);
 })
