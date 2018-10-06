9e254c377fc97c0867ca5306d07f01210701e785
rnnc <rohan_cy@outlook.com>
2018-10-06 12:54:52 -0400

    tidying up

---
+++ client/src/Components/Logos/LogoCard.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import classnames from 'classnames';
+
+export default class LogoCard extends Component {
+  state = {
+    selected: false
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      selected: this.state.selected ? false : true
+    })
+  }
+
+  render() {
+
+    const { selected } = this.state;
+
+    return (
+      <div
+        className={classnames("logoCard", { selected })}
+        onClick={this.handleClick.bind(this)}
+      >
+        {this.props.children}
+        <div className="info" hidden={!selected}>
+          yo
+        </div>
+      </div>
+    )
+  }
+}
