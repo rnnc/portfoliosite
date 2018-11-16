dea7d84c22d58a0731a95f78c74ffa7c2ba99e4e
rnnc <rnnc@github.com>
2018-11-16 09:33:53 -0500

    more styling changes

--- client/src/Components/End.jsx
+++ client/src/Components/End.jsx
@@ -7,7 +7,7 @@ export default class End extends Component {
   render() {
     const githubLink = "https://github.com/rnnc/portfoliosite";
     return (
-      <Observer thershold={ 1 }>
+      <Observer thershold={ 0.1 }>
         { ({ inView, ref }) => {
           const visible = inView ? 'visible' : '';
           return (
@@ -16,23 +16,28 @@ export default class End extends Component {
               <div className="background" />
 
               <div className="windowContainer" ref={ ref }>
+                <div className="window_bg" />
                 <div className={ `window ${visible}` }>
-                  <div className="text">
-                    { "Want to see the code for this site?" }
+                  <div className="window_bg" />
+                  <div className="textBox">
+                    <div className="text">
+                      <span>Want to see The Code for this Site?</span>
+                      <span>Click Here</span>
+                    </div>
+                    <a className="gh_icon" href={ githubLink } rel="noopener noreferrer" target="_blank">
+                      <div className="iconContainer">
+                        <GithubIcon />
+                      </div>
+                    </a>
                   </div>
                 </div>
               </div>
 
-              <a className="gh_icon" href={ githubLink } rel="noopener noreferrer" target="_blank">
-                <div className="iconContainer">
-                  <GithubIcon />
-                </div>
-              </a>
+
 
             </div>
           )
         } }
-
       </Observer>
     );
   }
